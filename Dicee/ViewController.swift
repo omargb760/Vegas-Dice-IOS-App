f2818058c39dcf16c2fdd785027aafd55d259096
Omar Gonzalez <omargb22@gmail.com>
2019-04-28 19:32:51 -0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Omar Gonzalez on 4/28/19.
+//  Copyright © 2019 Omar Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
